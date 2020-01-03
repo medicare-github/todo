b006e7d1df68ad47c53413609b18c5f80e36f632
medicare <mcareducation@gmail.com>
2020-01-03 08:41:08 +0700

    fixing commit

---
+++ precache-manifest.507805d8ad7981785fc1a8086ab2d05b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a649f595b9d5af5c67e32ca71c72b37",
+    "url": "/medicare/index.html"
+  },
+  {
+    "revision": "32523dc4e6d8c554670f",
+    "url": "/medicare/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a2f6a90fc4c9d591e4eb",
+    "url": "/medicare/static/js/2.a250f35c.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/medicare/static/js/2.a250f35c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "32523dc4e6d8c554670f",
+    "url": "/medicare/static/js/main.6477140b.chunk.js"
+  },
+  {
+    "revision": "3d85f5405dc401e1d625",
+    "url": "/medicare/static/js/runtime-main.1410bfdd.js"
+  }
+]);
